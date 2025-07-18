012a4ae1dd27bb13ca5aabe59cbfba7b22cef7de
Jared G <jared@droove.net>
2025-07-18 17:35:46 +1000

    Pulesaudio error

--- hosts/utm-vm/configuration.nix
+++ hosts/utm-vm/configuration.nix
@@ -60,7 +60,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
+  #services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
